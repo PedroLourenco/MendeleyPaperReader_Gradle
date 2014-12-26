ece4b91125259adc3d7e9399c2db0e04338669f7
pedrolourenco <pdrolourenco@gmail.com>
2014-12-26 11:33:32 +0000

    Continuous integration with calabash test

---
+++ app/src/calabash/test/features/login.feature
@@ -0,0 +1,22 @@
+Feature: Test log in and log out functions
+
+  @login_test
+  Scenario: As a logged in user I can add a file
+    Given I wait for the Sign in button to appear
+    Then I wait for the Mendeley oauth login page
+    Then I wait tjo see "Authorize"
+    Then I enter "pdrolourenco@gmail.com" into the "username" input field
+    Then I enter "000000" into the "password" input field
+    Then I touch "Authorize"
+    Then I wait for the "Contacting Mendeley ..." progress dialog to close
+    Then I wait for the "Sync data..." progress dialog to close
+    Then Validate if you are in the right activity
+
+
+  @logOut_test
+  Scenario: As a logged in user I can perform a logout
+    Given I see logout button
+    Then I touch on "Logout" button
+    Then I confirm that i want to logout
+
+
---
+++ app/src/calabash/test/features/step_definitions/calabash_steps.rb
@@ -0,0 +1,80 @@
+require 'calabash-android/calabash_steps'
+
+
+
+Given(/^I wait for the Sign in button to appear$/) do
+  touch "button text:'Sign in'"
+end
+
+
+Then(/^I wait for the Mendeley oauth login page$/) do
+  wait_for( timeout: 30 ) { query( "webview" ) }
+  wait_for( timeout: 30 ) { query( "webview css:'*'" ).length != 3 }
+end
+
+Then(/^I wait tjo see "(.*?)"$/) do |arg1|
+  wait_for_elements_exist("webView css:'#username'")
+end
+
+
+Then(/^I enter "([^"]*)" into the "([^"]*)" input field$/) do |value, label_name|
+  enter_text("webView css:'##{label_name}'",value)
+end
+
+
+Then(/^I touch "([^"]*)"$/) do |arg|
+  touch("webView css:'.controls'")
+end
+
+
+Then(/^I wait for the "([^"]*)" progress dialog to close$/) do |text|
+
+  if text.eql? 'Sync data...'
+    # wait for dialog
+    unless query("TextView {text CONTAINS '#{text}'}").length == 1
+
+      wait_for(timeout: 90) { 1 == query("TextView {text CONTAINS '#{text}'}").length }
+    end
+  end
+
+
+  unless query("TextView {text CONTAINS '#{text}'}").length == 0
+    # If it does, then wait for it to close...
+    wait_for(timeout: 60) { 0 == query("TextView {text CONTAINS '#{text}'}").length }
+  end
+
+end
+
+=begin
+Then(/^I can see "(.*?)" on folder list$/) do |arg1|
+
+  preferences = get_preferences("MendeleyPaperReaderPREF")
+
+
+  printf('tokens'  + preferences.keys.to_s)
+
+  if !preferences.has_key? 'access_token'
+    fail(msg="Error. Login fail.")
+
+  end
+end
+=end
+
+
+Then(/^Validate if you are in the right activity$/) do
+  query("TextView text:'My Publications'")
+end
+
+Then(/^I touch on "([^"]*)" button$/) do |text|
+
+  touch "TextView text:'#{text}'"
+end
+
+Given(/^I see logout button$/) do
+  system "#{default_device.adb_command} shell input keyevent KEYCODE_MENU"
+end
+
+Then(/^I confirm that i want to logout$/) do
+  touch "button text:'OK'"
+end
+
