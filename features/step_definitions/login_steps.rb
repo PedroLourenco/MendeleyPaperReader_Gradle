9f9148f8f166277885c9b0904fe196b2316310c2
pedrolourenco <pdrolourenco@gmail.com>
2015-01-01 19:20:17 +0000

    travis timeout

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -4,24 +4,28 @@ require 'calabash-android/calabash_steps'
 
 Given(/^I wait for the Sign in button to appear$/) do
   touch "button text:'Sign in'"
+  puts('Step 1')
 end
 
 
 Then(/^I wait for the Mendeley oauth login page$/) do
   wait_for( timeout: 30 ) { query( "webview" ) }
   wait_for( timeout: 30 ) { query( "webview css:'*'" ).length != 3 }
+  
+  puts('Step 2')
 end
 
 
 
 Then(/^I enter "([^"]*)" into the "([^"]*)" input field$/) do |value, label_name|
   enter_text("webView css:'##{label_name}'",value)
+  puts('Step 3')
 end
 
 
 Then(/^I touch "([^"]*)"$/) do |arg|
   system "#{default_device.adb_command} shell input keyevent KEYCODE_ENTER"
-
+puts('Step 4')
 end
 
 
@@ -48,6 +52,8 @@ Then(/^I wait for the "([^"]*)" progress dialog to close$/) do |text|
      #If it does, then wait for it to close...
     wait_for(timeout: 10000) { 0 == query("TextView {text CONTAINS '#{text}'}").length }
   end
+  
+  puts('Step 5')
 
 end
 
@@ -56,18 +62,22 @@ Then(/^I validate if it's the right activity - MainMenuActivity$/) do
 
 
    wait_for(timeout: 90) { 1 == query("TextView text:'My Publications'").length}
+   puts('Step 6')
 end
 
 Then(/^I touch on "([^"]*)" button$/) do |text|
 
   touch "TextView text:'#{text}'"
+  puts('Step 7')
 end
 
 Given(/^I see logout button$/) do
   system "#{default_device.adb_command} shell input keyevent KEYCODE_MENU"
+  puts('Step 8')
 end
 
 Then(/^I confirm that i want to logout$/) do
   touch "button text:'OK'"
+  puts('Step 9')
 end
 
