868617d8f632fd850b298ba6b7ac24fc33bf379a
pedrolourenco <pdrolourenco@gmail.com>
2015-01-03 10:34:34 +0000

    travis timeout

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -4,28 +4,27 @@ require 'calabash-android/calabash_steps'
 
 Given(/^I wait for the Sign in button to appear$/) do
   touch "button text:'Sign in'"
-  #puts('Step 1')
+
 end
 
 
 Then(/^I wait for the Mendeley oauth login page$/) do
   wait_for( timeout: 30 ) { query( "webview" ) }
   wait_for( timeout: 30 ) { query( "webview css:'*'" ).length != 3 }
-  
-  #puts('Step 2')
+
 end
 
 
 
 Then(/^I enter "([^"]*)" into the "([^"]*)" input field$/) do |value, label_name|
   enter_text("webView css:'##{label_name}'",value)
-  #puts('Step 3')
+
 end
 
 
 Then(/^I touch "([^"]*)"$/) do |arg|
   system "#{default_device.adb_command} shell input keyevent KEYCODE_ENTER"
-#puts('Step 4')
+
 end
 
 
@@ -41,20 +40,10 @@ Then(/^I wait for the "([^"]*)" progress dialog to close$/) do |text|
   end
 
 
-  #q = query("TextView {text CONTAINS '#{text}'}")
-  #until q.empty?
-  #  puts('Sync data...');
-  #  q = query("TextView {text CONTAINS '#{text}'}")
-  #end
-
-
   unless query("TextView {text CONTAINS '#{text}'}").length == 0
      #If it does, then wait for it to close...
     wait_for(timeout: 10000) { 0 == query("TextView {text CONTAINS '#{text}'}").length }
   end
-  
-  #puts('Step 5')
-
 end
 
 
@@ -62,27 +51,23 @@ Then(/^I validate if it's the right activity - MainMenuActivity$/) do
 
 
    wait_for(timeout: 90) { 1 == query("TextView text:'My Publications'").length}
-   #puts('Step 6')
+
 end
 
 Then(/^I touch on "([^"]*)" button$/) do |text|
 
   touch "TextView text:'#{text}'"
-  #puts('Step 7')
+
 end
 
-Given(/^I see logout button$/) do
+Given(/^I see Menu list$/) do
   system "#{default_device.adb_command} shell input keyevent KEYCODE_MENU"
-  #puts('Step 8')
+
 end
 
 Then(/^I confirm that i want to logout$/) do
   touch "button text:'OK'"
-  #puts('Step 9')
-end
 
-Given(/^I see Settings button$/) do
-  system "#{default_device.adb_command} shell input keyevent KEYCODE_MENU"
 end
 
 Then(/^I validate if it's the right activity - Settings$/) do
@@ -123,7 +108,7 @@ end
 
 Then(/^I confirm that the file has not been downloaded$/) do
   wait_for(timeout: 60) {0 == query("ImageView tag:'open'").length}
-  #sleep(5)
+
 end
 
 Then(/^I uncheckBox checkbox "([^"]*)"$/) do |arg|
@@ -132,4 +117,8 @@ end
 
 Then(/^I sleep for (\d+) seconds$/) do |arg|
   sleep(5)
+end
+
+Then(/^I validate if it's the right activity \- AboutActivity$/) do
+  wait_for(timeout: 60) { 1 == query("TextView text:'Synchronization options'").length}
 end
--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -10,23 +10,13 @@ Before do |scenario|
     scenario = scenario.scenario_outline 
   end 
 
-  feature_name = scenario.feature.title
-  if FeatureNameMemory.feature_name != feature_name \
-      or ENV["RESET_BETWEEN_SCENARIOS"] == "1"
-    if ENV["RESET_BETWEEN_SCENARIOS"] == "1"
-      log "New scenario - reinstalling apps"
-    else
-      log "First scenario in feature - reinstalling apps"
-    end
+
     
     uninstall_apps
     install_app(ENV["TEST_APP_PATH"])
     install_app(ENV["APP_PATH"])
-    FeatureNameMemory.feature_name = feature_name
-	FeatureNameMemory.invocation = 1
-  else
-    FeatureNameMemory.invocation += 1
-  end
+
+
 end
 
 FeatureNameMemory = Class.new
